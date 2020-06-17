f60f6bcf1aeeb60b1a8989a70880adcd8ec404e4
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-17 14:08:57 +0200

    Installed Websocket Client (SockJS and StompJS)

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  private client: Client;
+
   constructor() { }
 
   ngOnInit() {
+
+	this.client = new Client();
+	this.client.webSocketFactory = ()=>{
+		return new SockJS("http://localhost:8080/chat-websocket");
+	}
+
+	this.client.onConnect = (frame) => {
+		console.log('Conectados: ' + this.client.connected + ' : ' + frame);
+	}
+
+	this.client.activate();
+
   }
 
 }
