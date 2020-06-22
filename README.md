f5206445a85dcd372b0178baa9dffc742193ae1e
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-22 08:20:23 +0200

    Finished project v1.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ChatApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.19.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.0.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('chat-app');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('chat-app app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to chat-app!');
   });
 });
